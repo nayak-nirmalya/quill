57428c6571240f1a0bbad2e42fba744557a31d49
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-08 21:17:26 +0530

    added cn util fn and compoent: MaxWidthWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
+    <MaxWidthWrapper className="bg-red-600">
       <div>Hello World!</div>
     </MaxWidthWrapper>
   );
