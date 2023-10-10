8f282bffae4937fa25c0585fba34f09fde83d639
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-10 20:36:40 +0530

    added title and subtittle for ladnig page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,14 @@ export default function Home() {
           Quill is now public!
         </p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        Chat with your <span className="text-blue-600">documents</span> is
+        seconds.
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        Quill allows you to have conversations with any PDF document. Simply
+        upload your file and start asking questions right away.
+      </p>
     </MaxWidthWrapper>
   );
 }
