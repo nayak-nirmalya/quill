09abc6232fa2867cdd79a079e6a96cca279dcc53
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-21 13:53:47 +0530

    redirect to dashboard after pushing user to db

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,22 +1,41 @@
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
 import React from "react";
+import { Loader2 } from "lucide-react";
 
 import { trpc } from "../_trpc/client";
 
-export const Page = () => {
+export default function Page() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  const {} = trpc.authCallback.useQuery(undefined, {
+  trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       if (success) {
         // User is synced with DB.
         router.push(origin ? `/${origin}` : "/dashboard");
       }
     },
+
+    onError: (err) => {
+      if (err.data?.code === "UNAUTHORIZED") {
+        router.push("/sign-in");
+      }
+    },
+
+    retry: true,
+    retryDelay: 500,
   });
 
-  return <div>Page</div>;
-};
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You&apos;ll be redirected authomatically.</p>
+      </div>
+    </div>
+  );
+}
