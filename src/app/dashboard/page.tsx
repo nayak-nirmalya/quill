5bc5815aa9cc34cd32ac887746a8ab71b84a1c69
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-21 14:03:02 +0530

    added new component: Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import { redirect } from "next/navigation";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Page() {
+import { db } from "@/db";
+import { Dashboard } from "@/components/Dashboard";
+
+export default async function Page() {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  return <div>{user.id}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return <Dashboard />;
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import React from "react";
+
+export function Dashboard() {
+  return <main className="mx-auto max-w-7xl md:p-10"></main>;
+}
