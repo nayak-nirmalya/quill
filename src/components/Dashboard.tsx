f272a27f8b86439a0db744ebf88436fff600df51
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-22 19:59:22 +0530

    instaslled pkg: date-fns

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import React from "react";
-import { Ghost } from "lucide-react";
+import { Ghost, Plus } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
+import { format } from "date-fns";
 
 import { UploadButton } from "./UploadButton";
 import { trpc } from "@/app/_trpc/client";
@@ -19,7 +21,43 @@ export function Dashboard() {
 
       {/* Display User Files */}
       {files && files.length !== 0 ? (
-        <div></div>
+        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
+          {files
+            .sort(
+              (a, b) =>
+                new Date(b.createdAt).getTime() -
+                new Date(b.createdAt).getTime()
+            )
+            .map((file) => (
+              <li
+                key={file.id}
+                className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg"
+              >
+                <Link
+                  href={`/dashboard/${file.id}`}
+                  className="flex flex-col gap-2"
+                >
+                  <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
+                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                    <div className="flex-1 truncate">
+                      <div className="flex items-center space-x-3">
+                        <h3 className="truncate text-lg font-medium text-zinc-900">
+                          {file.name}
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+
+                <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+                  <div className="flex items-center gap-2">
+                    <Plus className="h-4 w-4" />
+                    {format(new Date(file.createdAt), "MMM yyyy")}
+                  </div>
+                </div>
+              </li>
+            ))}
+        </ul>
       ) : isLoading ? (
         <Skeleton height={100} className="my-2" count={3} />
       ) : (
