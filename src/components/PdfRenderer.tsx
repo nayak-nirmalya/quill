7c4152935155af50adcfe9efa0eed16c8690e65d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-23 18:23:30 +0530

    installed pkg: react-dropzone

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -10,5 +10,11 @@ import {
 } from "lucide-react";
 
 export function PdfRenderer({ url }: { url: string }) {
-  return <div>PdfRenderer</div>;
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5"></div>
+      </div>
+    </div>
+  );
 }
