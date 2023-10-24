59859bb9e8b5a731a577c4b888bdcfbc3a872e62
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-24 12:34:45 +0530

    modifed shadcn progress to add custom indicator

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Dropzone from "react-dropzone";
-import { Cloud, File } from "lucide-react";
+import { Cloud, File, Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
@@ -118,9 +118,18 @@ function UploadDropzone() {
               {isUploading ? (
                 <div className="w-full mt-4 max-w-xs mx-auto">
                   <Progress
+                    indicatorColor={
+                      uploadProgress === 100 ? "bg-green-500" : ""
+                    }
                     value={uploadProgress}
-                    className="h-1 w-full bg-zinc-400"
+                    className="h-1 w-full bg-zinc-200"
                   />
+                  {uploadProgress === 100 ? (
+                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                      <Loader2 className="h-3 w-3 animate-spin" />
+                      Redirecting...
+                    </div>
+                  ) : null}
                 </div>
               ) : null}
 
