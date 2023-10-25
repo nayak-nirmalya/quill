32e410f1bf903c697b1c718689ee2886bf0264a7
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-25 19:34:04 +0530

    created new component: ChatInput, Messages

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function ChatInput() {
+  return (
+    <div className="absolute bottom-0 left-0 w-full">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Messages } from "./Messages";
+import { ChatInput } from "./ChatInput";
 
 export function ChatWrapper() {
-  return <div>ChatWrapper</div>;
+  return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 justify-between flex flex-col mb-28">
+        <Messages />
+      </div>
+
+      <ChatInput />
+    </div>
+  );
 }
