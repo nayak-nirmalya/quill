d790b7b36fbc4d9bba5beabab2d778b012dbc89f
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-25 20:22:19 +0530

    added react-textarea-autosize

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -34,7 +34,7 @@ export default async function Page({
         </div>
 
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper />
+          <ChatWrapper fileId={fileId} />
         </div>
       </div>
     </div>
