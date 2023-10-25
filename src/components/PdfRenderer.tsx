66475abd9ebc10c1c7aabfa7587d18768a2149cd
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-25 11:56:41 +0530

    added avatar, card, textarea, tooltip

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -15,6 +15,7 @@ import { z } from "zod";
 import SimpleBar from "simplebar-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useResizeDetector } from "react-resize-detector";
 
 import { useToast } from "./ui/use-toast";
 import { Button } from "./ui/button";
