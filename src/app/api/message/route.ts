f573b3c179d1e68850aa3248a2ccc3a9acfef1fd
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-26 15:03:14 +0530

    installed vercel ai pkg

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -2,10 +2,12 @@ import { NextRequest } from "next/server";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { db } from "@/db";
 import { pinecone } from "@/lib/pinecone";
+import { openai } from "@/lib/openai";
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
@@ -63,10 +65,55 @@ export const POST = async (req: NextRequest) => {
     take: 6,
   });
 
-  const formattedMessages = prevMessages.map((msg) => ({
+  const formattedPrevMessages = prevMessages.map((msg) => ({
     role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
     content: msg.text,
   }));
 
-  // const response = await ;
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content:
+          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+  \n----------------\n
+  
+  PREVIOUS CONVERSATION:
+  ${formattedPrevMessages.map((message) => {
+    if (message.role === "user") return `User: ${message.content}\n`;
+    return `Assistant: ${message.content}\n`;
+  })}
+  
+  \n----------------\n
+  
+  CONTEXT:
+  ${results.map((r) => r.pageContent).join("\n\n")}
+  
+  USER INPUT: ${message}`,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion) {
+      await db.message.create({
+        data: {
+          text: completion,
+          isUserMessage: false,
+          fileId,
+          userId,
+        },
+      });
+    },
+  });
+
+  return new StreamingTextResponse(stream);
 };
