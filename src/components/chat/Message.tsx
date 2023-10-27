a91c01969d32ad4aac291bdf822a72bfc391ed8c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-27 20:28:14 +0530

    added react-markdown

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 import { cn } from "@/lib/utils";
 import { ExtendedMessage } from "@/types/message";
@@ -16,7 +17,34 @@ export function Message({
         "justify-end": message.isUserMessage,
       })}
     >
-      Message
+      <div
+        className={cn(
+          "relative flex h-6 w-6 aspect-square items-center justify-center",
+          {
+            "order-2 bg-blue-600 rounded-sm": message.isUserMessage,
+            "order-1 bg-zinc-800 rounded-sm": !message.isUserMessage,
+            invisible: isNextMessageSamePerson,
+          }
+        )}
+      ></div>
+
+      <div
+        className={cn("flex flex-col space-y-2 text-base max-w-md mx-2", {
+          "order-1 items-end": message.isUserMessage,
+          "order-2 items-start": !message.isUserMessage,
+        })}
+      >
+        <div
+          className={cn("px-4 py-2 rounded-lg inline-block", {
+            "bg-blue-600 text-white": message.isUserMessage,
+            "bg-gray-200 text-gray-900": !message.isUserMessage,
+            "rounded-br-none":
+              !isNextMessageSamePerson && message.isUserMessage,
+            "rounded-bl-none":
+              !isNextMessageSamePerson && !message.isUserMessage,
+          })}
+        ></div>
+      </div>
     </div>
   );
 }
