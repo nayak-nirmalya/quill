fd61dc268b68256aa0b313d3afbc786e9a10537a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-28 18:57:08 +0530

    updated stripe api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,7 @@ import { PLANS } from "@/config/stripe";
 import { db } from "@/db";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2023-10-16",
+  apiVersion: "2023-08-16",
   typescript: true,
 });
 
